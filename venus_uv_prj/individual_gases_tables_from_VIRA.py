ffab26372c68610ac250b7d5e4feb689f7840dda
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2021-08-14 21:30:03 +0300

    files with all gaseous profiles were a bit formatted -- added number of lines in header; completed adding all absorption cross sections according to Haus2015; Modified UV_H2O.f90 in order to launch for every gas

--- venus_uv_prj/individual_gases_tables_from_VIRA.py
+++ venus_uv_prj/individual_gases_tables_from_VIRA.py
@@ -23,32 +23,34 @@ vira_height, vira_concentration, vira_temperature, vira_pressure = np.loadtxt (v
 gases_names = ["CO2/co2.hq", "H2O/h2o.hq", "CO/co.hq", "SO2/so2.hq", "OCS/ocs.hq", "HF/hf.hq", "HCl/hcl.hq"]
 #i = 0 # 0-CO2, 1-H2O, 2-CO, 3-SO2, 4-OCS, 5-HF, 6-HCl
 
-for p in gases_names:
-        f = "./Atmosph_Models/" + p
-        pp = p.split('/')[0]
-        ppp = pp.split('.')[0]
-        height, mixratio = np.loadtxt (f, skiprows=1, unpack=True)
+#for p in gases_names:
+p = "H2O/h2o.hq"    
+f = "./Atmosph_Models/" + p
+pp = p.split('/')[0]
+ppp = pp.split('.')[0]
+height, mixratio = np.loadtxt (f, skiprows=1, unpack=True)
 
         #print (height)
         #print (mixratio)
 
-        mix_rat = interp1d(height, mixratio)
+mix_rat = interp1d(height, mixratio)
     
-        gas_h = vira_height #height
-        gas_p = mix_rat(gas_h)*1e-6*vira_pressure #pressure
-        gas_t = vira_temperature #temperature
-        gas_c = mix_rat(gas_h)*1e-6*vira_concentration*1e5 #concentration
+gas_h = vira_height #height
+#        gas_p = mix_rat(gas_h)*1e-6*vira_pressure #pressure
+gas_p = vira_pressure/1000/1.013
+gas_t = vira_temperature #temperature
+gas_c = mix_rat(gas_h)*1e-6*vira_concentration*1e5 #concentration
     
-        save_file = "./Atmosph_Models/"+ pp + "/" + ppp + "_gas_profile.dat"
-        header =  "Haus2015\n" + str(len(gas_h)) + "\n" + pp + " #height(km), pressure(mbar), temperature(K), concentration (molecules/cm^2*km)"
+save_file = "./Atmosph_Models/"+ pp + "/" + ppp + "_gas_profile.dat"
+header =  "Haus2015\n" + str(len(gas_h)) + "\n" + pp + " #height(km), pressure(atm), temperature(K), concentration (molecules/cm^2*km)"
         
-        np.savetxt (save_file, np.transpose ((gas_h, gas_p, gas_t, gas_c)), header=header, delimiter='  ', comments='', fmt=fmt)
+np.savetxt (save_file, np.transpose ((gas_h, gas_p, gas_t, gas_c)), header=header, delimiter='  ', comments='', fmt=fmt)
         
-        index60 = np.where(gas_h==60.)[0][0]
+index60 = np.where(gas_h==60.)[0][0]
         
-        save_file60 = "./Atmosph_Models/"+ pp + "/" + ppp + "_gas_profile_60km.dat"
-        header60 = "Haus2015\n" + str(len(gas_h[index60:])) + "\n" + pp + " #height(km), pressure(mbar), temperature(K), concentration (molecules/cm^2*km)"
-        np.savetxt (save_file60, np.transpose((gas_h[index60:], gas_p[index60:], gas_t[index60:], gas_c[index60:])), header=header60, delimiter='  ', comments='', fmt=fmt)
+save_file60 = "./Atmosph_Models/"+ pp + "/" + ppp + "_gas_profile_60km.dat"
+header60 = "Haus2015\n" + str(len(gas_h[index60:])) + "\n" + pp + " #height(km), pressure(atm), temperature(K), concentration (molecules/cm^2*km)"
+np.savetxt (save_file60, np.transpose((gas_h[index60:], gas_p[index60:], gas_t[index60:], gas_c[index60:])), header=header60, delimiter='  ', comments='', fmt=fmt)
         
         
 #x = np.linspace(0, 100, num=85, endpoint=True) #vary right border to 80 or 100 km if error
