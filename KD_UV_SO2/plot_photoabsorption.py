87cdcc785a051d0655967e93e2d9c74c03057583
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2021-11-08 14:41:49 +0300

    recent commit

--- KD_UV_SO2/plot_photoabsorption.py
+++ KD_UV_SO2/plot_photoabsorption.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 path0 = "./RUN_KD_DO/UV_absorption/"
+path1 = "/home/taskerbliss/fortran/venus_uv/UV_absorption"
 
 def acs_read(dat_path):
     
@@ -37,14 +38,22 @@ def acs_read(dat_path):
               
 def uv_acs_plot_nm():
     
-    plt.plot(acs_read(path0+"SO2_ManattLane1993.dat")[0], acs_read(path0+"SO2_ManattLane1993.dat")[1], color='b', label='ManattLane1993')
-
-    plt.xlim([200, 400])
-    plt.grid(color='g', linestyle='-', linewidth=0.5)
+    plt.plot(acs_read(path0+"SO2_ManattLane1993.dat")[0], acs_read(path0+"SO2_ManattLane1993.dat")[1], color='b', label='SO2')
+    plt.plot(acs_read(path1+"/CO2/" + "CO2_aggregate.dat")[0], acs_read(path1 + "/CO2/" + "CO2_aggregate.dat")[1], color ='k', label='CO2')
+    plt.plot(acs_read(path1+"/H2O/" + "H2O_JPL2011.dat")[0], acs_read(path1 + "/H2O/" + "H2O_JPL2011.dat")[1], color ='c', label='H2O')
+    plt.plot(acs_read(path1+"/HCl/" + "HCl_Inn1975.dat")[0], acs_read(path1 + "/HCl/" + "HCl_Inn1975.dat")[1], color ='goldenrod', label='HCl')
+    plt.plot(acs_read(path1+"/OCS/" + "OCS_Molina1981.dat")[0], acs_read(path1+"/OCS/" + "OCS_Molina1981.dat")[1], color = 'lightcoral', label='OCS')
+    
+    plt.axvline(x=125, color = 'slategrey')
+    plt.axvline(x=200, color = 'slategrey')
+    plt.axvline(x=300, color = 'slategrey')
+    
+    plt.xlim([100, 400])
+    #plt.grid(color='g', linestyle='-', linewidth=0.5)
     plt.yscale('log')
     plt.xlabel ("wavelength [nm]")
     plt.ylabel ("ACS [cm^2/molec]")
-    plt.title ("SO2 photoabsorption cross section")
+    plt.title ("Venus atmospheric gases photoabsorption cross sections")
     plt.legend()
     plt.show()    
 
@@ -62,8 +71,8 @@ def uv_acs_plot_wavenum():
     plt.legend()
     plt.show()  
 
-#uv_acs_plot_nm()
-uv_acs_plot_wavenum()
+uv_acs_plot_nm()
+#uv_acs_plot_wavenum()
 
 
 
